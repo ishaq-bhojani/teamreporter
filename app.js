edc862b9e5b933c6a3ace2b9d044d8ebe86af9f0
Zeeshan Hassan <zeeshanhassanpk@hotmail.com>
2015-05-08 15:15:39 +0500

    changes for heroku

--- app.js
+++ app.js
@@ -50,11 +50,5 @@ app.use(function (err, req, res, next) {
         error: {}
     });
 });
-app.set('port', process.env.PORT || 5000);
-
-var server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
-
-});
 
 module.exports = app;
