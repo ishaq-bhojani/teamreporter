b71210d1e1d24c8df46378d536d6b7ecc5648413
Zeeshan Hassan <zeeshanhassanpk@hotmail.com>
2015-05-08 12:47:40 +0500

    changed server file

--- app.js
+++ app.js
@@ -50,6 +50,11 @@ app.use(function (err, req, res, next) {
         error: {}
     });
 });
+app.set('port', process.env.PORT || 3000);
 
+var server = app.listen(app.get('port'), function () {
+    debug('Express server listening on port ' + server.address().port);
+
+});
 
 module.exports = app;
